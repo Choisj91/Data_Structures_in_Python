a0847390a90ae82081594c801aa7861e64c8f100
SeungJae <stopj1123@gmail.com>
2021-06-01 00:15:01 +0900

    New Start with Python

--- section03.py
+++ section03.py
@@ -8,3 +8,4 @@ test_dict = {'1': 95, '4': 77, '3': 65, '5': 100, '2': 88}
 
 #simplejson 실행
 print(json.dumps(test_dict, sort_keys=True, indent=4 * ' '))
+print('Hello It is time to study Data Structure!')
