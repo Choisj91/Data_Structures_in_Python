21e21406e31e836af85193cb66af4505bcd6483a
SeungJae <stopj1123@gmail.com>
2021-06-09 00:20:48 +0900

    Array Samples

--- Chapter01.py
+++ Chapter01.py
@@ -1,5 +1,9 @@
 # Ctrl + Shift + B -> Run Build Task
 
+# 1차원 배열: 리스트로 구현시
 data_list = [1, 2, 3, 4, 5]
 print(data_list)
 
+# 2차원 배열: 리스트로 구현시
+data_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(data_list)
