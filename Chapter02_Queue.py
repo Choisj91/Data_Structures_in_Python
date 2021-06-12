930e2ab9fcc39ba40c32f2297bea6d548ea8676a
SeungJae <stopj1123@gmail.com>
2021-06-12 10:46:24 +0900

    Chapter02 Queue practice

---
+++ Chapter02_Queue.py
@@ -0,0 +1,17 @@
+# 대표적인 데이터 구조4: 큐 (Queue)
+
+# 큐 구조
+# 줄을 서는 행위와 유사
+# 가장 먼저 넣은 데이터를 가장 먼저 꺼낼 수 있는 구조
+# 음식점에서 가장 먼저 줄을 선 사람이 제일 먼저 음식점에 입장하는 것과 동일
+# FIFO(First-In, First-Out) 또는 LILO(Last-In, Last-Out) 방식으로 스택과 꺼내는 순서가 반대
+
+import queue
+
+data_queue = queue.Queue()
+
+data_queue.put("funcoding")
+data_queue.put(1)
+
+data_queue.qsize()
+print(data_queue.qsize())
